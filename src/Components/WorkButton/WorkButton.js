f4d3723320bd7dfba13bdded86a383b6d873652b
Svitlana <devop.edu@gmail.com>
2021-09-29 18:12:43 +0200

    changed dropdown-list styles

--- src/Components/WorkButton/WorkButton.js
+++ src/Components/WorkButton/WorkButton.js
@@ -1,32 +1,35 @@
-import React from 'react';
-import { Dropdown } from 'react-bootstrap';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { Dropdown } from "react-bootstrap";
 
 export default function WorkButton(props) {
+  const history = useHistory();
 
-	return (
-		<>
-			<div
-				className="w-100 mt-1 justify-content-center"
-				id="work-buttons-dropdown"
-			>
-				<Dropdown>
-					<Dropdown.Toggle
-						className="w-100 rounded-pill shadow-sm p-3"
-						variant="light"
-						id="dropdown-basic"
-					>
-						Välj yrke
-					</Dropdown.Toggle>
+  return (
+    <>
+      <div className="w-100 mt-1 justify-content-center work-button_container">
+        <Dropdown>
+          <Dropdown.Toggle
+            className="w-100 h5 rounded-pill shadow-sm p-3 occupation-btn"
+            id="dropdown-basic"
+            // drop="down"
+          >
+            Välj yrke
+          </Dropdown.Toggle>
 
-					<Dropdown.Menu className="w-100">
-						{props.occupation.occupations.map((job, i) => (
-							<Dropdown.Item href="/emely-chat" key={i} className="w-100" >
-                            {job}
-							</Dropdown.Item>
-						))}
-					</Dropdown.Menu>
-				</Dropdown>
-			</div>
-		</>
-	);
+          <Dropdown.Menu className="w-100 work-button_dropdown-list">
+            {props.occupation.occupations.map((job, i) => (
+              <Dropdown.Item
+                onClick={() => history.push("/emely-chat")}
+                key={i}
+                className="w-100 my-3 h5 work-button_dropdown-item"
+              >
+                {job}
+              </Dropdown.Item>
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+    </>
+  );
 }
