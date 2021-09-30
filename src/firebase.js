3f7d1526ffc469763b70a0f843b6f02a8b31ffc0
Dino Ferizovic <74234240+dife55@users.noreply.github.com>
2021-09-30 17:03:37 +0200

    feat: added new project and added firestore connection

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app"
 import "firebase/auth"
+import "firebase/firestore"
 
 
 /* Variables used from the .env.local file */
@@ -10,8 +11,9 @@ const app = firebase.initializeApp({
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID 
 })
 
 export const auth = app.auth()
+export const db = app.firestore()
+
 export default app
