8a116858cf986cf40e5ff21f25d4b29ab062dc9c
Dino Ferizovic <74234240+dife55@users.noreply.github.com>
2021-10-07 23:33:02 +0200

    feat: profile page with added features

--- src/Navigation/Profile/Profile.js
+++ src/Navigation/Profile/Profile.js
@@ -1,21 +1,35 @@
-import React, { useEffect } from 'react';
-import { Col, Card, Container, Row } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Col, Card, Container, Row, Alert, Button } from 'react-bootstrap';
 import UserMenu from '../../Components/UserMenu/UserMenu';
+import ProfileCard from '../../Components/ProfileCard/ProfileCard';
+import { useHistory } from 'react-router-dom';
 
 /* Icon imports */
 import { AiOutlineHome } from 'react-icons/ai';
 import { AiOutlineEdit } from 'react-icons/ai';
 import { BiLogOutCircle } from 'react-icons/bi';
+import { BiShowAlt } from 'react-icons/bi';
+import { FaRegTimesCircle } from 'react-icons/fa';
 
 import { useAuth } from '../../contexts/AuthContext';
 
 export default function Profile() {
-	const { currentUser, userDetails, getUserDetails } = useAuth();
+	const { currentUser, userDetails, logout } = useAuth();
+	const [error, setError] = useState('');
+	const history = useHistory();
 
-	/* Getting the current user details on mount */
-	useEffect(() => {
-		getUserDetails(currentUser.uid)
-	}, [currentUser.uid, getUserDetails])
+	async function handleLogout() {
+		setError('');
+
+		try {
+			await logout();
+			history.push('/login');
+
+			/* Catch error */
+		} catch (error) {
+			setError(error.message);
+		}
+	}
 
 	return (
 		<>
@@ -23,83 +37,56 @@ export default function Profile() {
 				<Row>
 					<UserMenu />
 				</Row>
-				<div>
-				</div>
-					
-				<Card className="mt-5 shadow-sm" id="profile-card">
-					<Row>
-
-						{/* The Side menu on the profile card */}
-						<Col xs={3} id="side-menu">
-							<Row className="mt-5">
-								<Col xs={2}>
-									<AiOutlineHome size={20} />
-								</Col>
-								<Col>Min profil </Col>
-							</Row>
-							<hr />
-							<Row>
-								<Col xs={2}>
-									<AiOutlineEdit size={20} />
-								</Col>
-								<Col>Redigera profil</Col>
-							</Row>
-							<hr />
-							<Row className="mb-5">
-								<Col xs={2}>
-									<BiLogOutCircle size={20} />
-								</Col>
-								<Col>Logga ut</Col>
-							</Row>
-						</Col>
-
-						{/* Profile fields */}
-						<Col className="ms-3">
-							
-							{/* Username */}
-							<Row className="mt-5">
-								<Col xs={4}>
-									Användarnamn
-								</Col>
-								<Col>{userDetails && userDetails.username}</Col>
-							</Row>
-							<hr />
-
-							{/* Email */}
-                            <Row>
-                            <Col xs={4}>
-                                Email
-                            </Col>
-                            <Col>{currentUser.email}</Col>
-                        </Row>
-                        <hr />
-							<Row>
-								<Col xs={4}>
-									Födelsedatum
-								</Col>
-								<Col>{userDetails && userDetails.birth_year}</Col>
-							</Row>
-							<hr />
-
-							{/* Native language */}
-							<Row >
-								<Col xs={4}>
-									Modersmål
-								</Col>
-								<Col>{userDetails && userDetails.native_language}</Col>
-							</Row>
-                            <hr />
-
-							{/* Current occupation */}
-                            <Row className="mb-5">
-                            <Col xs={4}>
-                                Syselsättning
-                            </Col>
-                            <Col>{userDetails && userDetails.current_occupation}</Col>
-                        </Row>
-						</Col>
+				<h2 className="text-center mb-4 mt-5 fw-bold">Användarkonto</h2>
+				<ProfileCard title={'Mina uppgifter'} buttonText={'Redigera'} linkTo={'/update-profile'} buttonIcon={<AiOutlineEdit className="me-2" size={15} />}>
+					<Row className="m-3">
+						<small className="fw-bold p-0">Användarnamn</small>
+						{userDetails && userDetails.username}
 					</Row>
-				</Card>
+					<Row className="m-3">
+						<small className="fw-bold p-0">E-postadress</small>
+						{currentUser.email}
+					</Row>
+					<Row className="m-3">
+						<small className="fw-bold p-0">Födelsedatum</small>
+						{userDetails && userDetails.birth_year}
+					</Row>
+					<Row className="m-3">
+						<small className="fw-bold p-0">Modersmål</small>
+						{userDetails && userDetails.native_language}
+					</Row>
+					<Row className="m-3">
+						<small className="fw-bold p-0">Sysselsättning</small>
+						{userDetails && userDetails.current_occupation}
+					</Row>
+					<Row className="text-end mb-3 pe-3">
+						<span>
+							<Button
+								variant="outline-success"
+								className="rounded-pill pe-3 ps-3 fw-bold register-btn_light"
+								id="log-out-button-profile"
+								onClick={handleLogout}
+							>
+								<BiLogOutCircle className="me-2" size={18} />
+								Logga ut
+							</Button>
+						</span>
+					</Row>
+				</ProfileCard>
+
+				<br/>
+				<ProfileCard title={'Samtalshistorik'} buttonText={'Visa'} linkTo={'/chat-history'} buttonIcon={<BiShowAlt className="me-2" size={15} />}>
+				
+				</ProfileCard>
+				<br/>
+				<ProfileCard title={'Radera användare'} buttonText={'Radera'} linkTo={'/delete-profile'} buttonIcon={<FaRegTimesCircle className="me-2" size={15} />}>
+				
+				</ProfileCard>
+
+				<br/>
+				<ProfileCard title={'Byt lösenord'} buttonText={'Redigera'} linkTo={'/change-password'} buttonIcon={<BiShowAlt className="me-2" size={15} />}>
+				
+				</ProfileCard>
 			</Container>
 		</>
 	);
