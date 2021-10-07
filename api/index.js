5a8ffa90a8a29c4fc4267e4100f68c9a380d115f
Svitlana <devop.edu@gmail.com>
2021-10-07 15:48:43 +0200

    feat:create dependencies for proxy-server

---
+++ api/index.js
@@ -0,0 +1,6 @@
+// dependensies:
+// 1. proxy-server (npm i react-amazing-proxy) = local launch BE and FE (npm start)
+// 2. express (npm install express) = middleware to create a robust API
+// 3. axios (npm i axios) = promise based HTTP client for the browser and node.js
+// 4. cors (npm i cors) = providing a Connect/Express middleware that can be used to enable CORS with various options.
+// 5. body-parser (npm i body-parser) = body parsing middleware
