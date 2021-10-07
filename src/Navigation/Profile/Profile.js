ef226a862334f3ce58f7e33d2e53aacbefa35833
Dino Ferizovic <74234240+dife55@users.noreply.github.com>
2021-10-07 08:11:28 +0200

    feat: render correct fields on the profile card

--- src/Navigation/Profile/Profile.js
+++ src/Navigation/Profile/Profile.js
@@ -1,38 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { db } from "../../firebase"
+import React, { useEffect } from 'react';
+import { Col, Card, Container, Row } from 'react-bootstrap';
+import UserMenu from '../../Components/UserMenu/UserMenu';
 
+/* Icon imports */
 import { AiOutlineHome } from 'react-icons/ai';
 import { AiOutlineEdit } from 'react-icons/ai';
 import { BiLogOutCircle } from 'react-icons/bi';
 
-import UserMenu from '../../Components/UserMenu/UserMenu';
-import { Col, Card, Container, Row } from 'react-bootstrap';
-
 import { useAuth } from '../../contexts/AuthContext';
 
 export default function Profile() {
-	const { currentUser } = useAuth();
-	const [user, setUser] = useState();
+	const { currentUser, userDetails, getUserDetails } = useAuth();
 
-	const getUser = async () => {
-		try {
-		  const documentSnapshot = await db
-			.collection('users-debug')
-			.doc(currentUser.uid)
-			.get();
-	
-		  const userData = documentSnapshot.data();
-		  setUser(userData);
-		} catch {
-		  
-		}
-	  };
-	  
-
+	/* Getting the current user details on mount */
 	useEffect(() => {
-		getUser();
-	
-	});
+		getUserDetails(currentUser.uid)
+	}, [currentUser.uid, getUserDetails])
 
 	return (
 		<>
@@ -45,6 +28,8 @@ export default function Profile() {
 					
 				<Card className="mt-5 shadow-sm" id="profile-card">
 					<Row>
+
+						{/* The Side menu on the profile card */}
 						<Col xs={3} id="side-menu">
 							<Row className="mt-5">
 								<Col xs={2}>
@@ -68,15 +53,19 @@ export default function Profile() {
 							</Row>
 						</Col>
 
+						{/* Profile fields */}
 						<Col className="ms-3 fw-bold">
 							
+							{/* Username */}
 							<Row className="mt-5">
 								<Col xs={4}>
 									Användarnamn
 								</Col>
-								<Col>Dino</Col>
+								<Col>{userDetails && userDetails?.username}</Col>
 							</Row>
 							<hr />
+
+							{/* Email */}
                             <Row>
                             <Col xs={4}>
                                 Email
@@ -88,21 +77,25 @@ export default function Profile() {
 								<Col xs={4}>
 									Födelsedatum
 								</Col>
-								<Col>1987-04-28</Col>
+								<Col>{userDetails && userDetails?.birth_year}</Col>
 							</Row>
 							<hr />
+
+							{/* Native language */}
 							<Row >
 								<Col xs={4}>
 									Modersmål
 								</Col>
-								<Col>{user && user?.native_language}</Col>
+								<Col>{userDetails && userDetails?.native_language}</Col>
 							</Row>
                             <hr />
+
+							{/* Current occupation */}
                             <Row className="mb-5">
                             <Col xs={4}>
                                 Syselsättning
                             </Col>
-                            <Col>Heltidsarbete</Col>
+                            <Col>{userDetails && userDetails?.current_occupation}</Col>
                         </Row>
 						</Col>
 					</Row>
