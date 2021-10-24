ec3f0146a3d5ffc034da4ae5b8d5d28b1e43cbb9
Dino Ferizovic <74234240+dife55@users.noreply.github.com>
2021-10-24 23:56:30 +0200

    feat: added continiational rendering for the user/guest-menu based on login credentials

--- src/Components/PrivateRoute.js
+++ src/Components/PrivateRoute.js
@@ -1,14 +1,26 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import { Route, Redirect } from "react-router-dom"
 import { useAuth } from "../contexts/AuthContext"
 import UserMenu from "./UserMenu/UserMenu";
+import GuestMenu from "./GuestMenu/GuestMenu";
 
 export default function PrivateRoute({ component: Component, ...rest }) {
   const { currentUser } = useAuth()
+  const [userEmail, setUserEmail] = useState('')
+
+  useEffect(() => {
+    if(currentUser.email === 'guest@emely.com'){
+
+      setUserEmail('guest@emely.com')
+    } else {
+      setUserEmail('')
+    }
+  }, [])
 
   return (
     <>
-    <UserMenu />
+  {/* Render GuestMenu if guest is logged in else render regular UserMenu on login */}
+    {userEmail !== '' ?  <GuestMenu /> : <UserMenu /> }
     <Route
       {...rest}
       render={props => {
