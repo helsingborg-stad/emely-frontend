119891a263afade3334cc642b97837742de557ca
Dino Ferizovic <74234240+dife55@users.noreply.github.com>
2021-11-17 08:49:15 +0100

    fix: removed live-text rendering

--- src/Navigation/WorkEmely/WorkEmely.js
+++ src/Navigation/WorkEmely/WorkEmely.js
@@ -1,72 +1,93 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Container, Row, Col, Modal, Button } from "react-bootstrap";
+import React, { useState, useEffect, useContext } from 'react';
+import { Container, Row, Col, Modal, Button } from 'react-bootstrap';
 
-import EmelyDialogue from "../../Components/EmelyDialogue/EmelyDialogue";
-import WorkButton from "../../Components/WorkButton/WorkButton";
-import PulseLoader from "react-spinners/PulseLoader";
-import { ConversationContext } from "../../contexts/ConversationContext";
-import useLiveTextDisplaying from "../../customHooks/useLiveTextDisplaying";
+import EmelyDialogue from '../../Components/EmelyDialogue/EmelyDialogue';
+import WorkButton from '../../Components/WorkButton/WorkButton';
+import PulseLoader from 'react-spinners/PulseLoader';
+import { ConversationContext } from '../../contexts/ConversationContext';
+import { useAuth } from '../../contexts/AuthContext';
 
 export default function WorkEmely(props) {
-  const { getButtons, jobButtons } = useContext(ConversationContext);
-  const { renderWords, click, ref } = useLiveTextDisplaying(
-    `Jag har arbetat med att intervjua folk för jobb i ungefär ett halvår. Eftersom att jag är nybörjare på detta blir jag nervös ibland och glömmer vad jag tidigare har sagt. Det är inte ditt fel om jag råkar glömma något. Vilket arbete söker du?`
-  );
+	const { userDetails, currentUser } = useAuth();
 
-  const [show, setShow] = useState(false);
+	const { getButtons, jobButtons, currentProgress, setCurrentProgress } =
+		useContext(ConversationContext);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+	const [show, setShow] = useState(false);
 
-  useEffect(() => {
-    getButtons();
-  }, []);
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
 
-  useEffect(() => {
-    renderWords();
-  }, [click]);
+	const [isLoading, setIsLoading] = useState();
 
-  return (
-    <>
-      <Container id="container-work-emely">
-        <Modal
-          scrollable={true}
-          className="work-button-modal"
-          size="md"
-          show={show}
-          onHide={handleClose}
-        >
-          <Modal.Body>
-            {jobButtons ? (
-              <WorkButton occupation={jobButtons}></WorkButton>
-            ) : (
-              <PulseLoader size={12} color={"gray"} />
-            )}
-          </Modal.Body>
-        </Modal>
+	useEffect(() => {
+		setIsLoading(true);
 
-        <EmelyDialogue className="m-0">
-          {
-            <p className="m-3 p-3 emely-dialog_dialogue-text">
-              {renderWords()}
-            </p>
-          }
-        </EmelyDialogue>
+		/* --- Render text after delay --- */
+		const timer = setTimeout(() => {
+			setIsLoading(false);
+		}, 1500);
+		return timer;
+	}, [currentUser]);
 
-        <Row className="p-0 mb-5">
-          <Col className="text-center mt-5">
-            <Button
-              variant="none"
-              className="w-100 register-btn clickBtn"
-              id="edit-button"
-              onClick={handleShow}
-              ref={ref}
-            >
-              Välj yrke
-            </Button>
-          </Col>
-        </Row>
-      </Container>
-    </>
-  );
+	useEffect(() => {
+		/* Reset current progress on new chat */
+		setCurrentProgress(0);
+	}, []);
+
+	useEffect(() => {
+		getButtons();
+	}, []);
+
+	return (
+		<>
+			<Container id="container-work-emely">
+				<Modal
+					className="work-button-modal"
+					size="md"
+					show={show}
+					onHide={handleClose}
+				>
+					<Modal.Body>
+						{jobButtons ? (
+							<WorkButton occupation={jobButtons}></WorkButton>
+						) : (
+							<PulseLoader size={12} color={'gray'} />
+						)}
+					</Modal.Body>
+				</Modal>
+
+				<EmelyDialogue className="m-0">
+					{isLoading ? (
+						<p className="m-3 p-5 emely-dialog_dialogue-text text-center">
+							<PulseLoader size={12} color={'gray'} />{' '}
+						</p>
+					) : (
+						<p className="m-3 p-3 emely-dialog_dialogue-text">
+							Jag har arbetat med att intervjua folk för jobb i ungefär ett
+							halvår. Eftersom att jag är nybörjare på detta blir jag nervös
+							ibland och glömmer vad jag tidigare har sagt. Det är inte ditt fel
+							om jag råkar glömma något.
+							<br />
+							<br />
+							Vilket arbete söker du?
+						</p>
+					)}
+				</EmelyDialogue>
+
+				<Row className="p-0 mb-5">
+					<Col className="text-center mt-5">
+						<Button
+							variant="none"
+							className="w-100 register-btn"
+							id="edit-button"
+							onClick={handleShow}
+						>
+							Välj yrke
+						</Button>
+					</Col>
+				</Row>
+			</Container>
+		</>
+	);
 }
