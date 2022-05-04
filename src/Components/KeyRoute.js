c4654bd5531ecf6071559e6aba4ce1a530bf001b
Dino Ferizovic <74234240+dife55@users.noreply.github.com>
2022-05-04 12:47:02 +0200

    fix: changed checking for key in routes

--- src/Components/KeyRoute.js
+++ src/Components/KeyRoute.js
@@ -6,17 +6,18 @@ import AlertMessage from './AlertMessage/AlertMessage';
 
 export default function KeyRoute({ component: Component, ...rest }) {
 	/* ------ Variables, Hooks & State ------ */
-	const { getKeys, allKeys, msg, setMsgVariant, setMsg } = useAuth();
-	const [isCorrectKey] = useState(sessionStorage.getItem('sessionKey'));
+	const {
+		getKeys,
+		allKeys,
+		msg,
+		setMsgVariant,
+		setMsg,
+		isCorrectKey,
+		checkKey,
+		correctKey,
+		setCorrectKey,
+	} = useAuth();
 	const history = useHistory();
-
-	useEffect( () => {
-		try {
-			getKeys();
-		} catch (error) {
-			console.log(error.code);
-		}
-	}, []);
 
 
 	/* --- Check if you are on fika or intervju and change background accordingly --- */
@@ -24,15 +25,17 @@ export default function KeyRoute({ component: Component, ...rest }) {
 		try {
 			/* --- If you are on fika --- */
 			if (window.location.href.indexOf('fika') > -1) {
-				const fikaBackground = document.body.style.background = 'var(--fika)';
+				const fikaBackground = (document.body.style.background = 'var(--fika)');
 				return fikaBackground;
 
 				/* --- If you are on intervju --- */
 			} else if (window.location.href.indexOf('intervju') > -1) {
-				const interviewBackground = document.body.style.background = 'var(--interview)';
+				const interviewBackground = (document.body.style.background =
+					'var(--interview)');
 				return interviewBackground;
 			} else {
-				const mainBackground = document.body.style.background = 'var(--mainBackground)';
+				const mainBackground = (document.body.style.background =
+					'var(--mainBackground)');
 				return mainBackground;
 			}
 		} catch (error) {
@@ -40,16 +43,41 @@ export default function KeyRoute({ component: Component, ...rest }) {
 		}
 	}, [window.location.href]);
 
+	/* Check if key matches database-key */
+	useEffect(() => {
+		const date = new Date();
+		var dateInSeconds = date.getTime() / 1000;
+
+		try {
+			allKeys.every((key) => {
+				if (key.key === sessionStorage.getItem('sessionKey')) {
+					if (key.deadline.seconds >= dateInSeconds) {
+						setCorrectKey(true);
+						return false;
+					} else {
+						setCorrectKey(false);
+						return false;
+					}
+				} else {
+					setCorrectKey(false);
+					return true;
+				}
+			});
+		} catch (error) {
+			console.log(error.message);
+		}
+	}, [window.location.href]);
 
 	return (
 		<>
-    {msg && <AlertMessage />}
+			{msg && <AlertMessage />}
 			<Route
 				{...rest}
 				render={(props) => {
-
-          {/* If key is correct render page else redirect back to home */}
-					return isCorrectKey !== "wrong key" ? <Component {...props} /> : <Redirect to="/" />;
+					{
+						/* If key is correct render page else redirect back to home */
+					}
+					return correctKey ? <Component {...props} /> : <Redirect to="/" />;
 				}}
 			></Route>
 		</>
